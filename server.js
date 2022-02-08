55232fc8bfc74d37c5b32a2c1e4f1c9bf27227df
Olawale Mayor <olawalemayor90@gmail.com>
2022-02-08 21:08:23 +0100

    fixed directory in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const ngApp = express();
 
-ngApp.use(express.static("./dist/LatestNews"));
+ngApp.use(express.static("./dist/under10bucks"));
 
 ngApp.get("/*", function (request, response) {
   response.sendFile(path.join(__dirname, "/dist/under10bucks/index.html"));
